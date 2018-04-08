ac4998fd6b990371e644280bf2a169b35eda0200
Wang Zhe <wangzhe.lynx@gmail.com>
2018-04-09 02:52:31 +0800

    LSI

---
+++ code/LSI_topic_modeling.py
@@ -0,0 +1,67 @@
+from utils import *
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+from pylab import *
+
+
+def LSI_model(input, n_components, n_iter):
+    norm_corpus = normalize_corpus(input)
+    vectorizer, _ = build_feature_matrix(norm_corpus, feature_type='tfidf')
+
+    svd_model = TruncatedSVD(n_components=n_components, n_iter=n_iter)
+    svd_transformer = Pipeline([('tfidf', vectorizer),
+                                ('svd', svd_model)])
+
+    svd_matrix = svd_transformer.fit_transform(norm_corpus)
+
+    return vectorizer, svd_model, svd_transformer, svd_matrix
+
+
+if __name__ == '__main__':
+    input_data = ["The fox jumps over the dog",
+                  "The fox is very clever and quick",
+                  "The dog is slow and lazy",
+                  "The cat is smarter than the fox and the dog",
+                  "Python is an excellent programming language",
+                  "Java and Ruby are other programming languages",
+                  "Python and Java are very popular programming languages",
+                  "Python programs are smaller than Java programs"]
+
+    vectorizer, svd_model, svd_transformer, svd_matrix = LSI_model(input_data, 2, 100)
+
+    # test_query = ['dog fox jump smarter cat']
+    test_query = ['program small java python', 'dog fox jump smarter cat']
+
+    norm_query = normalize_corpus(test_query)
+    query_vector = svd_transformer.transform(norm_query)
+    predicted_topic = np.argmax(query_vector, axis=1).tolist()
+
+    # print query_vector
+    print 'test query predicted topic: ', predicted_topic
+
+    feat_names = vectorizer.get_feature_names()
+
+    for compNum in range(len(svd_model.components_)):
+        print compNum
+        comp = svd_model.components_[compNum]
+
+        # Sort the weights in the first component, and get the indices
+        indices = np.argsort(comp).tolist()[::-1]
+
+        # Grab the top 10 terms which have the highest weight in this component.
+        terms = [feat_names[weightIndex] for weightIndex in indices[0:10]]
+        weights = [comp[weightIndex] for weightIndex in indices[0:10]]
+        print terms, weights
+        terms.reverse()
+        weights.reverse()
+        print terms, weights
+
+        positions = arange(10) + .5  # the bar centers on the y axis
+
+        figure(compNum)
+        barh(positions, weights, align='center')
+        yticks(positions, terms)
+        xlabel('Weight')
+        title('Strongest terms for component %d' % (compNum))
+        grid(True)
+        show()
---
+++ code/utils.py
@@ -0,0 +1,151 @@
+from contractions import CONTRACTION_MAP
+import re
+import nltk
+import string
+from nltk.stem import WordNetLemmatizer
+from HTMLParser import HTMLParser
+import unicodedata
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from pattern.text.en import tag
+from nltk.corpus import wordnet as wn
+
+
+stopword_list = nltk.corpus.stopwords.words('english')
+stopword_list = stopword_list + ['mr', 'mrs', 'come', 'go', 'get',
+                                 'tell', 'listen', 'one', 'two', 'three',
+                                 'four', 'five', 'six', 'seven', 'eight',
+                                 'nine', 'zero', 'join', 'find', 'make',
+                                 'say', 'ask', 'tell', 'see', 'try', 'back',
+                                 'also']
+wnl = WordNetLemmatizer()
+html_parser = HTMLParser()
+
+
+def expand_contractions(text, contraction_mapping):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+                                      flags=re.IGNORECASE | re.DOTALL)
+
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match) \
+            if contraction_mapping.get(match) \
+            else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char + expanded_contraction[1:]
+        return expanded_contraction
+
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+
+def build_feature_matrix(documents, feature_type='frequency'):
+    feature_type = feature_type.lower().strip()
+
+    if feature_type == 'binary':
+        vectorizer = CountVectorizer(binary=True, min_df=1,
+                                     ngram_range=(1, 1))
+    elif feature_type == 'frequency':
+        vectorizer = CountVectorizer(binary=False, min_df=1,
+                                     ngram_range=(1, 1))
+    elif feature_type == 'tfidf':
+        vectorizer = TfidfVectorizer(min_df=1,
+                                     ngram_range=(1, 1))
+    else:
+        raise Exception("Wrong feature type entered. Possible values: 'binary', 'frequency', 'tfidf'")
+
+    feature_matrix = vectorizer.fit_transform(documents).astype(float)
+
+    return vectorizer, feature_matrix
+
+
+from scipy.sparse.linalg import svds
+
+
+def low_rank_svd(matrix, singular_count=2):
+    u, s, vt = svds(matrix, k=singular_count)
+    return u, s, vt
+
+
+def pos_tag_text(text):
+    def penn_to_wn_tags(pos_tag):
+        if pos_tag.startswith('J'):
+            return wn.ADJ
+        elif pos_tag.startswith('V'):
+            return wn.VERB
+        elif pos_tag.startswith('N'):
+            return wn.NOUN
+        elif pos_tag.startswith('R'):
+            return wn.ADV
+        else:
+            return None
+
+    tagged_text = tag(text)
+    tagged_lower_text = [(word.lower(), penn_to_wn_tags(pos_tag))
+                         for word, pos_tag in
+                         tagged_text]
+    return tagged_lower_text
+
+
+def lemmatize_text(text):
+    pos_tagged_text = pos_tag_text(text)
+    lemmatized_tokens = [wnl.lemmatize(word, pos_tag) if pos_tag
+                         else word
+                         for word, pos_tag in pos_tagged_text]
+    lemmatized_text = ' '.join(lemmatized_tokens)
+    return lemmatized_text
+
+
+def tokenize_text(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    return tokens
+
+def remove_special_characters(text):
+    tokens = tokenize_text(text)
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_tokens = filter(None, [pattern.sub(' ', token) for token in tokens])
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def remove_stopwords(text):
+    tokens = tokenize_text(text)
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def keep_text_characters(text):
+    filtered_tokens = []
+    tokens = tokenize_text(text)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def normalize_corpus(corpus, lemmatize=True,
+                     only_text_chars=False,
+                     tokenize=False):
+    normalized_corpus = []
+    for text in corpus:
+        text = html_parser.unescape(text)
+        text = expand_contractions(text, CONTRACTION_MAP)
+        if lemmatize:
+            text = lemmatize_text(text)
+        else:
+            text = text.lower()
+        text = remove_special_characters(text)
+        text = remove_stopwords(text)
+        if only_text_chars:
+            text = keep_text_characters(text)
+
+        if tokenize:
+            text = tokenize_text(text)
+            normalized_corpus.append(text)
+        else:
+            normalized_corpus.append(text)
+
+    return normalized_corpus
